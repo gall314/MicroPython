0ae9bb07f237a390a21a8fdf2384d5fb73dd20bd
GregBus <grzegorz.bus@dibby.pl>
2023-01-28 15:16:13 -0500

    move forward

--- samples/code-sample-class.py
+++ samples/code-sample-class.py
@@ -1,6 +1,7 @@
 from machine import Pin, PWM
 import utime
 
+
 class Engine:
     def __int__(self, forward, backward, speed):
         self.forward = forward
@@ -10,8 +11,8 @@ class Engine:
         speed.duty_u16(int(65535 * 0.5))
 
     def forward(self):
-       self.backward.off()
-       self.forward.on()
+        self.backward.off()
+        self.forward.on()
 
 
 led = Pin(25)
@@ -19,3 +20,4 @@ led.on()
 left_engine = Engine(Pin(0), Pin(2), PWM(Pin(4)))
 right_engine = Engine(Pin(5), Pin(18), PWM(Pin(19)))
 
+left_engine.forward()
